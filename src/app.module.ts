a78bcf1cac068ea5fd96a84c033eea5f211c6f23
Gabriele Ferreri <megasoft78@gmail.com>
2022-06-16 11:50:00 +0200

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,11 +11,7 @@ import { ScheduledJob } from './entities/scheduled-job.entity';
     ConfigModule.forRoot(),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: 5432,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
+      url: process.env.DATABASE_URL,
       ssl: true,
       entities: [ScheduledJob, Job],
       synchronize: true,
